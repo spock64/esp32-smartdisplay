9f20f1f3211fbdb904e3074eca6069f29aaabde2
j <spock@rogersfamily.me.uk>
2023-08-08 17:09:42 +0100

    again

--- src/tft_st7789.cpp
+++ src/tft_st7789.cpp
@@ -114,7 +114,7 @@ void ST7789_send_init_commands()
 
   //------------------------------display and color format setting--------------------------------//
 
-  static const uint8_t cmd_madctl = {0x00};
+  static const uint8_t cmd_madctl[] = {0x00};
   ST7789_send_command(CMD_MADCTL, cmd_madctl, 1);
 
   //  JLX240 display datasheet
@@ -123,52 +123,54 @@ void ST7789_send_init_commands()
   //writedata(0x0A);
   //writedata(0x82);
 
-static const uint8_t cmd_ramctrl = {0x00, 0xE0};
+static const uint8_t cmd_ramctrl[] = {0x00, 0xE0};
   ST7789_send_command(CMD_RAMCTRL, cmd_ramctrl , 2);
   //writedata(0x00);
   //writedata(0xE0); // 5 to 6 bit conversion: r0 = r5, b0 = b5
 
-  ST7789_send_command(CMD_COLMOD, {0x55}, 1);
+static const uint8_t cmd_colmod[] = {0x55};
+  ST7789_send_command(CMD_COLMOD, cmd_colmod, 1);
   //writedata(0x55);
   delay(10);
 
   //--------------------------------ST7789V Frame rate setting----------------------------------//
-  ST7789_send_command(CMD_PORCTRL, {0x0c, 0x0c, 0x00, 0x33, 0x35}, 5);
+  static const uint8_t cmd_porctl[] = {0x0c, 0x0c, 0x00, 0x33, 0x35};`
+  ST7789_send_command(CMD_PORCTRL, (const uinit8_t*) {0x0c, 0x0c, 0x00, 0x33, 0x35}, 5);
   // writedata(0x0c);
   // writedata(0x0c);
   // writedata(0x00);
   // writedata(0x33);
   // writedata(0x33);
 
-  ST7789_send_command(CMD_GCTRL, {0x35}, 1);      // Voltages: VGH / VGL
+  ST7789_send_command(CMD_GCTRL, (const uinit8_t*) {0x35}, 1);      // Voltages: VGH / VGL
   //writedata(0x35);
 
   //---------------------------------ST7789V Power setting--------------------------------------//
-  ST7789_send_command(CMD_VCOMS, {0x28}, 1);
+  ST7789_send_command(CMD_VCOMS, (const uinit8_t*) {0x28}, 1);
   //writedata(0x28);		// JLX240 display datasheet
 
-  ST7789_send_command(CMD_LCMCTRL, {0x0c}, 1);
+  ST7789_send_command(CMD_LCMCTRL, (const uinit8_t*) {0x0c}, 1);
   //writedata(0x0C);
 
-  ST7789_send_command(CMD_VDVVRHEN, {0x01, 0xFF}, 2);
+  ST7789_send_command(CMD_VDVVRHEN, (const uinit8_t*) {0x01, 0xFF}, 2);
   // writedata(0x01);
   // writedata(0xFF);
 
-  ST7789_send_command(CMD_VRHS, {0x10}, 1);       // voltage VRHS
+  ST7789_send_command(CMD_VRHS, (const uinit8_t*) {0x10}, 1);       // voltage VRHS
   //writedata(0x10);
 
-  ST7789_send_command(CMD_VDVSET, {0x20}, 1);
+  ST7789_send_command(CMD_VDVSET, (const uinit8_t*) {0x20}, 1);
   //writedata(0x20);
 
-  ST7789_send_command(CMD_FRCTR2, {0x0f}, 1);
+  ST7789_send_command(CMD_FRCTR2, (const uinit8_t*) {0x0f}, 1);
   //writedata(0x0f);
 
-  ST7789_send_command(CMD_PWCTRL1, {0xa4, 0xa1}, 2);
+  ST7789_send_command(CMD_PWCTRL1, (const uinit8_t*) {0xa4, 0xa1}, 2);
   // writedata(0xa4);
   // writedata(0xa1);
 
+  ST7789_send_command(CMD_PVGAMCTRL, (const uinit8_t*) {0xd0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x32, 0x44, 0x42, 0x06, 0x12, 0x14, 0x17}, 14);
   //--------------------------------ST7789V gamma setting---------------------------------------//
-  ST7789_send_command(CMD_PVGAMCTRL, {0xd0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x32, 0x44, 0x42, 0x06, 0x12, 0x14, 0x17}, 14);
   // writedata(0xd0);
   // writedata(0x00);
   // writedata(0x02);
@@ -184,7 +186,7 @@ static const uint8_t cmd_ramctrl = {0x00, 0xE0};
   // writedata(0x14);
   // writedata(0x17);
 
-  ST7789_send_command(CMD_NVGAMCTRL, {0xd0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x31, 0x54, 0x47, 0x0e, 0x1c, 0x17, 0x1b, 0x1e}, 14);
+  ST7789_send_command(CMD_NVGAMCTRL, (const uinit8_t*) {0xd0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x31, 0x54, 0x47, 0x0e, 0x1c, 0x17, 0x1b, 0x1e}, 14);
   // writedata(0xd0);
   // writedata(0x00);
   // writedata(0x02);
@@ -202,13 +204,13 @@ static const uint8_t cmd_ramctrl = {0x00, 0xE0};
 
   ST7789_send_command(CMD_INVON);
 
-  ST7789_send_command(CMD_CASET, {0x00, 0x00, 0x00, 0xef}, 4);    // Column address set
+  ST7789_send_command(CMD_CASET, (const uinit8_t*) {0x00, 0x00, 0x00, 0xef}, 4);    // Column address set
   // writedata(0x00);
   // writedata(0x00);
   // writedata(0x00);
   // writedata(0xEF);    // 239
 
-  ST7789_send_command(CMD_RASET, {0x00, 0x00, 0x01, 0x3f}, 4);    // Row address set
+  ST7789_send_command(CMD_RASET, (const uinit8_t*) {0x00, 0x00, 0x01, 0x3f}, 4);    // Row address set
   // writedata(0x00);
   // writedata(0x00);
   // writedata(0x01);
